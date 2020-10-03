2aa456930718fd358f9c91a74c7d2f5f0541d41a
Jim <jim@arnellconsulting.com>
2020-10-03 10:32:31 +0200

    Initial verison

---
+++ site/build/precache-manifest.ec61a906cd46d0606beef928528a5099.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c527d02226f35ad1ac2e9907bfe68c1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12d61c73c7c5eebae90e",
+    "url": "/static/css/main.084b06d8.chunk.css"
+  },
+  {
+    "revision": "cf8641d6eefd38a53b76",
+    "url": "/static/js/2.5c130e90.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/static/js/2.5c130e90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12d61c73c7c5eebae90e",
+    "url": "/static/js/main.7cb39acb.chunk.js"
+  },
+  {
+    "revision": "92140938f21271b78561",
+    "url": "/static/js/runtime-main.f1889a16.js"
+  }
+]);
