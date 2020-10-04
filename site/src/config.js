c6a5e346af879bf55dd0f42f5d8830880a54e31f
Jim <jim@arnellconsulting.com>
2020-10-04 09:59:38 +0200

    .

--- site/src/config.js
+++ site/src/config.js
@@ -12,7 +12,9 @@ config.domains = {};
  * Add the domain from your serverless express.js back-end here.
  * This will enable your front-end to communicate with your back-end.
  * (e.g. 'https://api.mydomain.com' or 'https://091jafsl10.execute-api.us-east-1.amazonaws.com')
+ *
+ * Domain fetched from environment.
  */
-config.domains.api = "https://6mg3mrmc86.execute-api.us-east-1.amazonaws.com";
+config.domains.api = process.env.domain;
 
 export default config;
